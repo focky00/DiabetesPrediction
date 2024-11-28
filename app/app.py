720c84369e104cc0734a3152412a8cd547f83431
Suhas K M <127014804+suhaskm28@users.noreply.github.com>
2024-11-29 00:04:53 +0530

    Update app.py   h5py to pickle

--- app/app.py
+++ app/app.py
@@ -1,23 +1,19 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import h5py
 
 app = Flask(__name__)
 
-# Load the trained model from .h5 file
-model_h5_path = 'random_forest.h5'
-with h5py.File(model_h5_path, 'r') as h5_file:
-    model_byte_stream = h5_file['random_forest_model'][()]
-    model = pickle.loads(model_byte_stream.tobytes())
+# Load the trained model from .pkl file
+model_path = 'random_forest.pkl'  # Assuming the model is saved as a pickle file
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)  # Load the model with pickle
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 # Prediction route - handles form submission and prediction
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get input values from the form
@@ -27,13 +23,11 @@ def predict():
     skin_thickness = int(request.form.get('skin_thickness', 0))
     insulin = int(request.form.get('insulin', 0))
     bmi = float(request.form['bmi'])
-    diabetes_pedigree_function = float(
-        request.form.get('diabetes_pedigree_function', 0.0))
+    diabetes_pedigree_function = float(request.form.get('diabetes_pedigree_function', 0.0))
     age = int(request.form['age'])
 
     # Make prediction using the loaded model
-    input_data = np.array(
-        [[pregnancies, glucose, blood_pressure, bmi, diabetes_pedigree_function, age]])
+    input_data = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
     prediction = model.predict(input_data)
 
     # Prepare response
@@ -43,7 +37,6 @@ def predict():
         result = 'No Diabetes'
 
     return render_template('result.html', prediction=result)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
